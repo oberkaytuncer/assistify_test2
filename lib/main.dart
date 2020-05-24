88a1120e4c209fa97401eb6f01346f775535235d
Ömer Berkay Tuncer <oberkaytuncer@gmail.com>
2020-05-24 10:42:29 +0300

    version 1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_messaging_app/app/landing_page.dart';
+import 'package:flutter_messaging_app/locator.dart';
+import 'package:flutter_messaging_app/view_model/user_model.dart';
 import 'package:provider/provider.dart';
 
-import 'app/landing_page.dart';
-import 'locator.dart';
-import 'view_model/user_model.dart';
+
+
+
 
 void main() {
   setupLocator();
